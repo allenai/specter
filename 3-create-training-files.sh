ce14bd669a8ac22e69b91abe0b2dc5a76717abc6
Olli Jokinen <olli.jokinen@snowfox.ai>
2023-04-14 08:40:18 +0200

    fixed data.json content to match train,test,val

--- 3-create-training-files.sh
+++ 3-create-training-files.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -x
+
 echo "Creating training files.. "
 python specter/data_utils/create_training_files.py \
---data-dir data/training \
---metadata data/training/metadata.json \
---outdir data/preprocessed/
-echo "Done. See results is data/preprocessed/"
+  --data-dir data/training \
+  --metadata data/training/metadata.json \
+  --outdir data/preprocessed2/
+
+echo "Done. See results is data/preprocessed2/"
 
